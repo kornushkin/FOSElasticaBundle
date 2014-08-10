482fbd0ed241306c45b2f7a3c070c350c488c969
Aleksei Kornushkin <kornushkin@gmail.com>
2014-08-10 16:15:49 +0400

    Hybrid results now available through method findPaginated()

---
+++ Paginator/TransformedHybridPaginatorAdapter.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace FOS\ElasticaBundle\Paginator;
+
+use FOS\ElasticaBundle\Transformer\ElasticaToModelTransformerInterface;
+use Elastica\SearchableInterface;
+use Elastica\Query;
+
+/**
+ * Allows pagination of \Elastica\Query
+ * Hybrid results returns
+ */
+class TransformedHybridPaginatorAdapter extends RawPaginatorAdapter
+{
+    private $transformer;
+
+    /**
+     * @param SearchableInterface                 $searchable the object to search in
+     * @param Query                               $query the query to search
+     * @param array                               $options
+     * @param ElasticaToModelTransformerInterface $transformer the transformer for fetching the results
+     */
+    public function __construct(SearchableInterface $searchable, Query $query, array $options = array(), ElasticaToModelTransformerInterface $transformer)
+    {
+        parent::__construct($searchable, $query, $options);
+
+        $this->transformer = $transformer;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getResults($offset, $length)
+    {
+        return new TransformedHybridPartialResults($this->getElasticaResults($offset, $length), $this->transformer);
+    }
+}
